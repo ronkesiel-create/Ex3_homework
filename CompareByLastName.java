6187891730f743f25f16137caad4cf6e632641a2
ronkesiel-create <ron.kesiel@gmail.com>
2026-05-25 16:58:11 +0300

    Update CompareByLastName.java

--- CompareByLastName.java
+++ CompareByLastName.java
@@ -6,9 +6,13 @@ public class CompareByLastName implements Comparator<Student> {
      * @param o1 the first object to be compared.
      * @param o2 the second object to be compared.
      * @return 0 if equal, 1 if "o1"'s is greater, -1 otherwise
+     * @throws NullPointerException if One of the given students is null
      */
     @Override
     public int compare(Student o1, Student o2) {
+        if (o1 ==null ||  o2== null) {
+            throw new NullPointerException("One of the arguments is null");
+        }
         int result = o1.getLastName().compareTo(o2.getLastName());
         if (result == 0) return result;
         if (result >0) return 1;
